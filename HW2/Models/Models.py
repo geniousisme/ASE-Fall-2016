3a3a1594e9403d7af63da13f89bf22da47912aa2
RuiLu <rlu0213@hotmail.com>
2016-09-29 22:34:24 -0400

    Pass flake8 (Models.py)

--- HW2/Models/Models.py
+++ HW2/Models/Models.py
@@ -1,15 +1,18 @@
 from django.utils import simplejson
 from google.appengine.ext import db
 
+
 def event_key(group='default'):
     return db.Key.from_path('event', group)
+
 
 class JsonProperty(db.TextProperty):
     def validate(self, value):
         return value
 
     def get_value_for_datastore(self, model_instance):
-        result = super(JsonProperty, self).get_value_for_datastore(model_instance)
+        result = super(JsonProperty,
+                       self).get_value_for_datastore(model_instance)
         result = simplejson.dumps(result)
         return db.Text(result)
 
@@ -20,8 +23,9 @@ class JsonProperty(db.TextProperty):
             pass
         return super(JsonProperty, self).make_value_from_datastore(value)
 
+
 class Event(db.Model):
-    event_id   = db.StringProperty(required=True)
+    event_id = db.StringProperty(required=True)
     event_json = JsonProperty()
 
     @classmethod
@@ -30,14 +34,16 @@ class Event(db.Model):
 
     @classmethod
     def create(cls, event_id, event_json):
-        new_event = Event(parent=event_key(), event_id=event_id, event_json=event_json)
+        new_event = Event(parent=event_key(),
+                          event_id=event_id,
+                          event_json=event_json)
         new_event.put()
 
     @classmethod
     def update(cls, event_id, insert_event_json):
         event = Event.get_by_event_id(event_id)
         event_json_obj = simplejson.loads(event.event_json)
-        if event_json_obj.has_key("guestusr"):
+        if "guestusr" in event_json_obj:
             event_json_obj["guestusr"].append(insert_event_json)
         else:
             event_json_obj["guestusr"] = [insert_event_json]
